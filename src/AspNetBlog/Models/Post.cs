5751ca0ab5d73189c0ec8b3a6e353152cc945712
luke hammer <lhammer@multnomah.edu>
2016-01-20 13:43:14 -0800

    Dynamically control behavior with custom configuration

---
+++ src/AspNetBlog/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetBlog.Models
+{
+    public class Post
+    {   
+        public long Id { get; set; }
+
+        [Required]
+        [Display(Name = "Blog post title")]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 5,
+                        ErrorMessage = "Title must be between 5 and 100 character long")]
+        public string Title { get; set; }
+        public DateTime PostedDate { get; set; }
+        public string Author { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [MinLength(5, ErrorMessage = "Blog post must be at least 5 charaters long")]
+        public string Body { get; set; }
+    }
+}
